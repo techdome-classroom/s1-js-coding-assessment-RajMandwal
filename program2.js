0373c070a593352e889f40d72f0628a8a70f044a
RajMandwal <rajmandwal809@gmail.com>
2024-10-07 12:16:18 +0530

    Done

--- program2.js
+++ program2.js
@@ -1,7 +1,19 @@
 const decodeTheRing = function (s, p) {
+  let i = 0, j = 0;
+  while (i < s.length && j < p.length) {
+    if (p[j] === '*') {
+      while (j < p.length && p[j] === '*') j++;
+      if (j === p.length) return true;
+      while (i < s.length && (i === 0 || s[i - 1] !== s[i]) && s[i] !== p[j]) i++;
+    } else if (p[j] === '?') {
+      i++; j++;
+    } else if (s[i] === p[j]) {
+      i++; j++;
+    } else {
+      return false;
+    }
+  }
+  return i === s.length && j === p.length;
+};
 
-    // write your code here
-
-  };
-  
-  module.exports = decodeTheRing;
+module.exports = decodeTheRing;
